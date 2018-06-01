0eb3d33ba2c61de3e3e3a6607c8fd57f5fbe4271
ryuhey_biz <vc.ryuhey@gmail.com>
2018-06-01 23:01:30 +0900

    [add] units.py

--- ufloat.py
+++ ufloat.py
@@ -1,9 +1,40 @@
-class ufloat(float):
+from units import *
 
-    def __init__(self, x):
-        float.__init__(self, x)
+
+class ufloat(object):
+
+    def __init__(self, x, unit):
+        self.x = float(x)
+        self.unit = unit
+
+    def __str__(self):
+        """ Return string, joining value and unit. """
+        return str(self.x) + " " + str(self.unit)
+
+    def __float__(self):
+        """ Return only value for float."""
+        return float.__float__(self.x)
+
+    def __add__(self, other):
+        """
+        Value (without unit) adds. If other is not ufloat, transform it to
+        float and add. In case, unit is overwrite by ufloat's unit.
+        Adopted unit is self's.
+        :param other: ufloat or float
+        :return: ufloat
+        """
+        if other is ufloat:
+            distance = other.unit / self.unit
+        else:
+            distance = 1.0
+        try:
+            ans = float(other) * distance + self.x
+        finally:
+            pass
+        return ufloat(ans, self.unit)
 
 
 if __name__ == '__main__':
-    test = ufloat(100)
-    print(test)
+    self = ufloat(100, Unit)
+    other = ufloat(200, Unit)
+    print(self + other)
---
+++ units.py
@@ -0,0 +1,7 @@
+class Unit(object):
+
+    def __truediv__(self, other):
+        return 10.0
+
+    def __str__(self):
+        return 'N'
