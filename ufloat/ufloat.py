ba41dc55901fef3c1f17d05bbcfefcc824315d67
ryuhey_biz <vc.ryuhey@gmail.com>
2018-06-05 21:59:16 +0900

    [update] 冗長な表現を修正

--- ufloat/ufloat.py
+++ ufloat/ufloat.py
@@ -8,12 +8,12 @@ class ufloat(object):
         self.unit = unit
 
     def __str__(self):
-        """ str(),print()で呼び出される。値と単位を文字列に変換し,1スペースあけて出力 """
-        return str(self.x) + " " + str(self.unit)
+        """ str(),print()で呼び出される。値と単位を文字列に変換し出力 """
+        return '{} {}'.format(self.x, self.unit)
 
     def __float__(self):
-        """ float()で呼び出される。値をfloat型に変換し値のみ出力 """
-        return float.__float__(self.x)
+        """ float()で呼び出される。値のみ出力 """
+        return self.x
 
     def __add__(self, other):
         """
